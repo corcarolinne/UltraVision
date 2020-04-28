d80b6dd8b9fc5617d42cfe792d56948fda6401fa
Carolinne Cordeiro <carolinnemcordeiro@gmail.com>
2020-04-28 18:31:46 +0100

    Creating customer class

---
+++ src/model/Customer.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Customer {
+	
+	// properties
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String email;
+	private String phoneNumber;
+	private String cardNumber;
+	private String membership;
+	private int score;
+	
+	// constructor
+	public Customer(String firstName, String lastName, String address, String email, String phoneNumber, String cardNumber, String membership, int score) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.cardNumber = cardNumber;
+		this.membership = membership;
+		this.score = score;
+	}
+
+	// other methods
+}
