8627394ac8279719caf43669c094682f27024c34
Carolinne Cordeiro <carolinnemcordeiro@gmail.com>
2020-05-02 17:29:16 +0100

    Creating table and method to show rented titles

--- src/controllers/DashboardController.java
+++ src/controllers/DashboardController.java
@@ -1,8 +1,6 @@
 package controllers;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import models.DashboardModel;
 import models.Title;
 import views.DashboardView;
 
--- src/views/DashboardView.java
+++ src/views/DashboardView.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -14,7 +15,9 @@ public class DashboardView extends JFrame {
 	// properties
 	private DashboardController controller;
 	private JTable availableTitlesTable;
-	public String[][] titlesData;
+	private JTable rentedTitlesTable;
+	public String[][] availabletitlesData;
+	public String[][] rentedTitlesData;
     
 	
 	// constructor
@@ -24,11 +27,15 @@ public class DashboardView extends JFrame {
 		// basic window properties
 		this.setVisible(true);
         this.setSize(1500,550);
-        this.setTitle("Dashboard");
+        this.setTitle("Ultra Vision System");
         
         // adding panel
     	JPanel panel = new JPanel();
-    	this.add(panel);	
+    	this.add(panel);
+    	
+    	// creating label
+    	JLabel brandLabel = new JLabel("Ultra Vision"); 
+    	panel.add(brandLabel);
     	
     	// adding a button
     	JButton addRentButton = new JButton("Add Rent");
@@ -36,28 +43,43 @@ public class DashboardView extends JFrame {
     	addRentButton.setActionCommand("add-rent");
     	panel.add(addRentButton);
     	
+    	// creating label for table
+    	JLabel availableTitlesLabel = new JLabel("Available Titles"); 
+    	panel.add(availableTitlesLabel);
+    	
     	// creating header
     	 String[] header = {"Title", "Type","Year Of Release", "Genre", "Format", "Price", "Director", "Band"};
     	
     	// calling method from model to show available titles
-    	 titlesData = controller.titleModel.showAvailableTitles("","");
+    	 availabletitlesData = controller.titleModel.showAvailableTitles("","");
     	 
-    	 // 
-    	 availableTitlesTable = new JTable(titlesData, header);
+    	 // creating table and adding it to the panel
+    	 availableTitlesTable = new JTable(availabletitlesData, header);
          panel.add(availableTitlesTable);
     
          // scroll
          JScrollPane scroll = new JScrollPane(availableTitlesTable);
          panel.add(scroll);
          
+        // creating label for table
+     	JLabel rentedTitlesLabel = new JLabel("In Transit"); 
+     	panel.add(rentedTitlesLabel);
+     	
+     	// calling method from model to show available titles
+     	rentedTitlesData = controller.titleModel.showRentedTitles("","");
+     	 
+		 // creating table and adding it to the panel
+		 rentedTitlesTable = new JTable(rentedTitlesData, header);
+		 panel.add(rentedTitlesTable);
+     
+		 // scroll
+		 JScrollPane scroll2 = new JScrollPane(rentedTitlesTable);
+		 panel.add(scroll2);
          
     	// calling validation method
     	this.validation();
     	
 	}
-	
-	
-	
 	
     // validation and repainting
     private void validation(){
